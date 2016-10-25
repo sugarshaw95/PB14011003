2a94b6152716a6f6f44a180fe68d6f5aa3002019
Xiao--Su <sugarshaw951018@gmail.com>
2016-10-25 16:04:30 +0800

    add homework folder

---
+++ homework/5.1-(a).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=5;
+	if(a>5)
+	{
+		a=1;
+		break;
+	}
+	else a=2;
+	return 0;
+}
---
+++ homework/5.1-(b).cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("«Î ‰»În:%d",&n);
+	int n;
+	switch(n)
+	{
+		case 1:
+			printf("yes!");
+		case 1:
+			printf("no!");
+		case 2:
+			printf("ok!");
+		default:
+			printf("illegal!");
+	}
+}
